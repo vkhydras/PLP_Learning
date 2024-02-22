d49c88d728584576e0daf532f414113e8ec6daab
vkhydras <victorkimaru8@gmail.com>
2024-02-22 14:27:55 +0300

    first commit

---
+++ Python/user_input.py
@@ -0,0 +1,5 @@
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+location = input("Please enter your location: ")
+
+print(f"Hello {name}, you are {age} years old and live in {location}.")
