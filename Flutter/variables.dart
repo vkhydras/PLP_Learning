9c236f47a759b33f04d950e2f7469d08aca80631
vkhydras <victorkimaru8@gmail.com>
2024-03-09 14:16:11 +0300

    dartVariables

---
+++ Flutter/variables.dart
@@ -0,0 +1,28 @@
+void main() {
+  int anInteger = 10;
+  print('Integer example: $anInteger');
+  
+  double aDouble = 10.5;
+  print('Double example: $aDouble');
+  
+  String aString = "Hello, Dart!";
+  print('String example: $aString');
+  
+  List<String> aList = ['Apple', 'Banana', 'Cherry'];
+  print('List example: $aList');
+  
+  Map<String, dynamic> aMap = {
+    'id': 1,
+    'name': 'John Doe',
+    'isVerified': true
+  };
+  print('Map example: $aMap');
+  
+  print('\nDemonstrating the use of Dart data types:');
+  print('A whole number (int): $anInteger');
+  print('A floating-point number (double): $aDouble');
+  print('A string of text (String): $aString');
+  print('A list of strings (List<String>): $aList');
+  print('A map with string keys and dynamic values (Map<String, dynamic>): $aMap');
+}
+
