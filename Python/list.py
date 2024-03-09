02e9b6ef950e89b07a61d4842fe48033da52848e
vkhydras <victorkimaru8@gmail.com>
2024-03-09 13:51:28 +0300

    list.py

---
+++ Python/list.py
@@ -0,0 +1,16 @@
+my_list = []
+
+my_list.extend([12, 20, 30, 40])
+
+my_list.insert(1, 15)
+
+my_list.extend([50, 60, 70])
+
+my_list.pop()
+
+my_list.sort()
+
+index_of_30 = my_list.index(30)
+
+print(index_of_30)
+print(my_list)
